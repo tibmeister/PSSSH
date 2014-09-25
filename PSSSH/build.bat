fb67cb16fc29eafdb34c0977d24ad2056cee68ef
tibmeister <jody.whitlock@tiberiansun.us>
2014-09-24 22:57:37 -0500

    Bugfix
    Batchfile needed work

--- PSSSH/build.bat
+++ PSSSH/build.bat
@@ -1,4 +1,4 @@
-rmdir bin\
+rmdir bin\ /q /s
 
 %windir%\Microsoft.NET\Framework\v3.5\csc.exe /t:library /out:PSSSH.dll /o /nostdlib /noconfig /r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /r:.\Renci.SshNet.dll /r:.\Ionic.Zlib.dll /r:C:\windows\assembly\GAC_MSIL\System.Xml.Linq\3.5.0.0__b77a5c561934e089\System.Xml.Linq.dll /r:C:\windows\assembly\GAC_MSIL\System.Data.DataSetExtensions\3.5.0.0__b77a5c561934e089\System.Data.DataSetExtensions.dll /r:C:\windows\assembly\GAC_MSIL\System.Xml\2.0.0.0__b77a5c561934e089\System.Xml.dll /r:C:\windows\assembly\GAC_MSIL\System\2.0.0.0__b77a5c561934e089\System.dll /r:C:\windows\assembly\GAC_MSIL\System.Core\3.5.0.0__b77a5c561934e089\System.Core.dll /r:C:\windows\assembly\GAC_MSIL\System.Management\2.0.0.0__b03f5f7f11d50a3a\System.Management.dll /r:C:\windows\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll *.cs Properties\AssemblyInfo.cs
 
